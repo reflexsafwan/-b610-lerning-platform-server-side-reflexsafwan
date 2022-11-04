e946e610fd9287f5de4777f1963843673b0f9b57
reflexsafwan <reflex.safwan@gmail.com>
2022-11-04 15:44:46 +0600

    courses api added

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
+const courses = require("./data/courses.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
