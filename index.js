586210a95961561791b35473cbd474b47cf68d7b
reflexsafwan <reflex.safwan@gmail.com>
2022-11-04 15:43:09 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
